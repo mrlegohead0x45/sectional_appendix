f5c759f3664fc0a79c45115830985351a81e5d1b
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2026-02-01 13:53:23 +0000

    textobject grouping

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use std::io::Read;
 
 use flate2::read::{DeflateDecoder, ZlibDecoder};
 use image::ExtendedColorType;
-use pdf::file::FileOptions;
+use pdf::{
+    file::FileOptions,
+    font::{FontData, FontDescriptor, FontType, TFont},
+};
 
 use crate::page::{TextObject, text_objects};
 // use lopdf::{Document, Object, StringFormat};
@@ -17,7 +20,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .open("London North Western (South) Sectional Appendix December 2025.pdf")?;
 
     let pages = doc.pages();
-    let page = doc.get_page(3 - 1)?;
+    let page = doc.get_page(96 - 1)?;
     let ops = &page
         .contents
         .as_ref()
@@ -29,9 +32,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     objs.sort_by_key(|t| (-t.y, t.x));
     // objs.reverse();
 
-    for obj in objs.iter() {
+    // for obj in objs.iter() {
+    //     println!("{:?}", obj);
+    // }
+
+    let font = page
+        .resources()?
+        .fonts
+        .get(&objs[0].font_name)
+        .ok_or("Could not get font")?
+        .load(&doc.resolver())?;
+
+    let avg_width = match font.data {
+        FontData::TrueType(TFont {
+            font_descriptor: Some(FontDescriptor { avg_width, .. }),
+            ..
+        }) => Ok(avg_width),
+        _ => Err("Could not get font average glyph width"),
+    }?;
+
+    let grouped = group_textobjects(objs, avg_width);
+    for obj in grouped.iter() {
         println!("{:?}", obj);
     }
+
+    // println!("{:#?}", font);
+    // match font.data {
+    //     FontType::TrueType => {}
+    //     _ => println!("other type of font found"),
+    // }
+    // println!(
+    //     "{:?}",
+    //     font.widths(&doc.resolver())?
+    //         .ok_or("Could not get font widths")?
+    // );
+    // println!("{:?}", font.encoding().ok_or("Could not get font encoding"));
+    // println!("{:?}", font.d)
     // let objs =
 
     // let pages = doc.get_pages();
@@ -103,4 +139,42 @@ fn zlib_inflate(buf: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let mut inflated = Vec::<u8>::new();
     z.read_to_end(&mut inflated)?;
     Ok(inflated)
+}
+
+/// Attempt to group adjacent textobjects forming a single text block into one
+fn group_textobjects(objs: Vec<TextObject>, avg_width: f32) -> Vec<TextObject> {
+    let mut grouped = Vec::new();
+
+    let mut acc = None;
+    for obj in objs {
+        match acc {
+            None => acc = Some(obj),
+            Some(ref acc_obj) => {
+                let expected_width =
+                    (acc_obj.text.len() as f32) * acc_obj.font_size * avg_width / 1000.0;
+
+                // close enough
+                let diff_x = (*obj.x - *(acc_obj.x + expected_width)).abs();
+                let diff_y = (*obj.y - *acc_obj.y).abs();
+                // println!("{:?diff_x}")
+                dbg!(diff_x);
+                dbg!(diff_y);
+                dbg!(acc_obj);
+                dbg!(&obj);
+
+                if diff_x < 22.0 && diff_y < 0.05 {
+                    println!("grouped {:?} and {:?}", &acc_obj.text, obj.text);
+                    acc = Some(TextObject {
+                        text: acc_obj.text.clone() + &obj.text,
+                        ..acc_obj.clone()
+                    });
+                } else {
+                    grouped.push(acc_obj.clone());
+                    acc = Some(obj);
+                }
+            }
+        }
+    }
+
+    grouped
 }
--- src/page.rs
+++ src/page.rs
@@ -1,5 +1,8 @@
 use ord_subset::OrdVar;
-use pdf::content::{Matrix, Op, TextDrawAdjusted};
+use pdf::{
+    content::{Matrix, Op, TextDrawAdjusted},
+    primitive::Name,
+};
 
 pub(crate) fn text_objects(operations: &[Op]) -> impl Iterator<Item = TextObject> {
     TextObjectParser {
@@ -12,6 +15,8 @@ pub(crate) struct TextObject {
     pub x: OrdVar<f32>,
     pub y: OrdVar<f32>,
     pub text: String,
+    pub font_size: f32,
+    pub font_name: Name,
 }
 
 #[derive(Debug, Clone)]
@@ -25,6 +30,8 @@ impl Iterator for TextObjectParser<'_> {
     fn next(&mut self) -> Option<Self::Item> {
         let mut last_coords = None;
         let mut last_text = None;
+        let mut last_font_size = None;
+        let mut last_font_name = None;
 
         while let Some(op) = self.ops.next() {
             match op {
@@ -32,12 +39,15 @@ impl Iterator for TextObjectParser<'_> {
                     last_coords = None;
                     last_text = None;
                 }
+                Op::TextFont { size, name } => {
+                    last_font_size = Some(*size);
+                    last_font_name = Some(name.clone());
+                }
                 Op::SetTextMatrix {
                     matrix: Matrix { e, f, .. },
                 } => {
                     last_coords = Some((*e, *f));
                 }
-
                 Op::TextDraw { text } => last_text = Some(text.to_string_lossy()),
                 Op::TextDrawAdjusted { array } => {
                     last_text = Some(
@@ -51,11 +61,18 @@ impl Iterator for TextObjectParser<'_> {
                     );
                 }
                 Op::EndText => {
-                    if let (Some((x, y)), Some(text)) = (last_coords.take(), last_text.take()) {
+                    if let (Some((x, y)), Some(text), Some(font_size), Some(font_name)) = (
+                        last_coords.take(),
+                        last_text.take(),
+                        last_font_size.take(),
+                        last_font_name.take(),
+                    ) {
                         return Some(TextObject {
                             x: OrdVar::new(x),
                             y: OrdVar::new(y),
                             text,
+                            font_size,
+                            font_name,
                         });
                     }
                 }
