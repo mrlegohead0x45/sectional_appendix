c6b63066c6368cdc76e70c52369702669967ca4f
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2026-02-02 13:03:46 +0000

    things

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::{collections::HashMap, io::Read};
+use std::{collections::HashMap, io::Read, iter, ops::Deref};
 
-use adobe_cmap_parser::get_unicode_map;
-use anyhow::{Context, Error, Ok, Result, anyhow};
+use adobe_cmap_parser::{get_byte_mapping, get_unicode_map};
+use anyhow::{Context, Error, Ok, Result, anyhow, bail};
 use flate2::read::{DeflateDecoder, ZlibDecoder};
 use image::ExtendedColorType;
 use pdf::{
     // content::Op::{self, TextDrawAdjusted},
+    PdfError,
     file::FileOptions,
     font::{Font, FontData, FontDescriptor, TFont},
     object::{Lazy, PageRc, Resolve},
@@ -21,55 +22,19 @@ mod page;
 
 fn main() -> Result<()> {
     // let doc = Document::load("London North Western (South) Sectional Appendix December 2025.pdf")?;
-    let path = "London North Western (South) Sectional Appendix December 2025.pf";
+    let path = "London North Western (South) Sectional Appendix December 2025.pdf";
     let doc = FileOptions::cached()
         .open(path)
         .with_context(|| format!("Failed to open {:?}", path))?;
 
     let mut pages = doc.pages();
 
-    let mut iol_fake_page_no = "".to_string();
-    for page in pages.by_ref() {
-        let page = page?;
-
-        let strings = extract_grouped_strings(page, &doc.resolver())?;
-
-        if strings.contains(&"Table of Contents".to_string()) {
-            let iol_idx: usize = strings
-                .iter()
-                .position(|s| s == "Index of Locations")
-                .context("Could not find Index of Locations in table of contents")?;
-
-            iol_fake_page_no = strings[iol_idx + 1].clone();
-            break;
-        }
-    }
+    let iol_fake_page_no = get_iol_page_no(&doc.resolver(), &mut pages)
+        .context("Failed to find location of Index of Locations")?;
     println!("found iol page number {}", iol_fake_page_no);
 
-    let mut iol_strings = Vec::new();
-
-    for page in pages.by_ref() {
-        let page = page?;
-
-        let strings = extract_grouped_strings(page, &doc.resolver())?;
-
-        if *strings.last().context("No strings found in page")? == iol_fake_page_no {
-            println!("found iol");
-            iol_strings = strings[5..=strings.len() - 3].to_vec();
-            break;
-        }
-    }
-
-    for page in pages.by_ref() {
-        let page = page?;
-
-        let strings = extract_grouped_strings(page, &doc.resolver())?;
-        if strings[2] != "Location" {
-            break;
-        }
-
-        iol_strings.append(&mut strings[4..=strings.len() - 3].to_vec())
-    }
+    let iol_strings = get_iol_strings(&doc.resolver(), pages, iol_fake_page_no)
+        .context("Failed to extract Index of Locations")?;
     println!("{:?}", iol_strings);
 
     // image::save_buffer(
@@ -82,7 +47,7 @@ fn main() -> Result<()> {
 
     // println!(
     //     "{:?}",
-    //     extract_grouped_strings(doc.get_page(99 - 1)?, &doc.resolver())?[9]
+    //     extract_grouped_strings(doc.get_page(19)?, &doc.resolver())?
     // );
 
     // let page = doc.get_page(99 - 1)?;
@@ -104,11 +69,75 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn zlib_inflate(buf: &[u8]) -> Result<Vec<u8>> {
-    let mut z = ZlibDecoder::new(buf);
-    let mut inflated = Vec::<u8>::new();
-    z.read_to_end(&mut inflated)?;
-    Ok(inflated)
+fn get_iol_strings(
+    resolver: &impl Resolve,
+    mut pages: impl Iterator<Item = Result<PageRc, PdfError>>,
+    iol_fake_page_no: String,
+) -> Result<Vec<String>, Error> {
+    let mut iol_strings = Vec::new();
+
+    for (page_no, page) in pages.by_ref().enumerate() {
+        let page = page.with_context(|| format!("Failed to get page {}", page_no))?;
+
+        let strings = extract_grouped_strings(page, resolver)
+            .with_context(|| format!("Failed to extract strings on page {}", page_no))?;
+
+        if *strings
+            .last()
+            .context("No strings found in page")
+            .context("Failed to skip to Index of Locations")?
+            == iol_fake_page_no
+        {
+            // println!("found iol");
+            iol_strings = strings[5..=strings.len() - 3].to_vec();
+            break;
+        }
+    }
+    for (page_no, page) in pages.by_ref().enumerate() {
+        let page = page.with_context(|| format!("Failed to get page {}", page_no))?;
+
+        let strings = extract_grouped_strings(page, resolver)
+            .with_context(|| format!("Failed to extract grouped strings on page {}", page_no))?;
+        if strings[2] != "Location" {
+            break;
+        }
+
+        iol_strings.append(&mut strings[4..=strings.len() - 3].to_vec())
+    }
+    Ok(iol_strings)
+}
+
+fn get_iol_page_no(
+    resolver: &impl Resolve,
+    pages: &mut impl Iterator<Item = Result<PageRc, PdfError>>,
+) -> Result<String> {
+    let mut iol_fake_page_no = "".to_string();
+    for (page_no, page) in pages.by_ref().enumerate() {
+        // {
+        let page = page.with_context(|| format!("Failed to get page {}", page_no))?;
+
+        let strings = extract_grouped_strings(page, resolver)
+            .with_context(|| format!("Failed to extract grouped strings on page {}", page_no))?;
+
+        if strings.contains(&"Table of Contents".to_string()) {
+            let iol_idx: usize = strings
+                .iter()
+                .position(|s| s == "Index of Locations")
+                .with_context(|| {
+                    format!(
+                        "Could not find Index of Locations in table of contents on page {}",
+                        page_no
+                    )
+                })?;
+
+            iol_fake_page_no = strings[iol_idx + 1].clone();
+            break;
+        };
+        // Ok(())
+        // }
+        // .with_context(|| format!("Failed on page {}", page_no))?;
+    }
+    Ok(iol_fake_page_no)
 }
 
 /// Attempt to group adjacent textobjects forming a single text block into one
@@ -158,16 +187,19 @@ fn extract_grouped_strings(page: PageRc, resolver: &impl Resolve) -> Result<Vec<
         .context("Failed to get page contents")?
         .operations(resolver)?;
     let mut objs: Vec<RawTextObject> = text_objects(ops)
-        // .map(|to| {
-        //     println!("{:?}", to.text);
-        //     to
-        // })
+        .map(|to| {
+            println!("{:?}", to.text);
+            to
+        })
         .collect();
 
     // sort top to bottom left to right (pdfs have y increasing upwards)
     objs.sort_by_key(|t| (-t.y, t.x));
 
-    let fonts = &page.resources()?.fonts;
+    let fonts = &page
+        .resources()
+        .context("Failed to get page resources")?
+        .fonts;
     // .get(&objs[0].font_name)
     // .ok_or("Could not get font")?
     // .load(&resolver)?;
@@ -182,8 +214,14 @@ fn extract_grouped_strings(page: PageRc, resolver: &impl Resolve) -> Result<Vec<
 
     let unicodified = objs
         .iter()
-        .map(|raw| unicodify(raw, fonts, resolver))
-        .collect::<Result<Vec<UnicodeTextObject>>>()?;
+        .map(|raw| {
+            let o = unicodify(raw, fonts, resolver)
+                .with_context(|| format!("Failed to unicodify text object {:?}", raw));
+            dbg!(&o);
+            o
+        })
+        .collect::<Result<Vec<UnicodeTextObject>>>()
+        .context("Failed to unicodify text objects")?;
 
     let grouped = group_textobjects(unicodified)
         .into_iter()
@@ -196,12 +234,6 @@ fn extract_grouped_strings(page: PageRc, resolver: &impl Resolve) -> Result<Vec<
     Ok(grouped)
 }
 
-// enum Stage {
-//     SeekingFirstToc,
-//     SeekingIndexOfLocations,
-//     Extracting
-// }
-
 fn unicodify(
     raw: &RawTextObject,
     fonts: &HashMap<Name, Lazy<Font>>,
@@ -209,11 +241,11 @@ fn unicodify(
 ) -> Result<UnicodeTextObject> {
     let font = fonts
         .get(&raw.font_name)
-        .context("Failed get font")?
+        .context("Failed to get font")?
         .load(resolver)?;
 
     // println!("{:?}", font);
-    // println!("{:?}", font.load(resolver));
+    // println!("{:?,}", font.load(resolver));
     let font_avg_width = match font.data {
         FontData::TrueType(TFont {
             font_descriptor: Some(FontDescriptor { avg_width, .. }),
@@ -223,24 +255,50 @@ fn unicodify(
         _ => 0.521,
     };
     let text = match &font.to_unicode {
-        None => Ok(String::from_utf8(raw.text.clone())?),
+        None => Ok(String::from_utf8(raw.text.clone())
+            .with_context(|| format!("Invalid UTF8 when no ToUnicode was found"))
+            .with_context(|| format!("Font was {:?}", font))?),
+
         Some(u) => {
-            let map: HashMap<u32, Vec<u8>> = get_unicode_map(&u.data().data(resolver)?)
+            let cmap_bytes = u.deref().data(resolver).context("Failed to get CMAP")?;
+            let map: HashMap<u32, Vec<u8>> = get_unicode_map(&cmap_bytes)
                 .map_err(Error::msg)
                 .context("Failed to parse CMAP")?;
-            let map: HashMap<Vec<u8>, &Vec<u8>> =
-                HashMap::from_iter(map.iter().map(|(cid, v)| (vec![0, *cid as u8], v)));
+            let byte_mapping = get_byte_mapping(&cmap_bytes)
+                .map_err(Error::msg)
+                .context("Failed to parse CMAP")?;
+            // dbg!(byte_mapping);
+            let width = byte_mapping
+                .codespace
+                .first()
+                .context("No codespace found in CMAP")?
+                .width as usize
+                + 1;
+            let map: HashMap<Vec<u8>, &Vec<u8>> = HashMap::from_iter(map.iter().map(|(cid, v)| {
+                (
+                    {
+                        let mut cid = vec![*cid as u8];
+                        cid.splice(..0, iter::repeat(0u8).take(width - cid.len()));
+                        cid
+                    },
+                    v,
+                )
+            }));
+            // dbg!(&map);
             let text = String::from_utf16(
                 &raw.text
-                    .chunks(2)
+                    .chunks(width)
                     .map(|cid| {
-                        let b = map.get(cid);
+                        let mut cid = cid.to_vec();
+                        cid.splice(..0, iter::repeat(0u8).take(width - cid.len()));
+                        // .collect::<Vec<u8>>();
+                        // dbg!(&cid);
+                        let b = map.get(&cid);
                         match b {
                             None => {
-                                return Err(anyhow!(
-                                    "Failed to get unicode for character {:?}",
-                                    cid
-                                ));
+                                dbg!(&cid);
+                                println!("{}", String::from_utf8(cmap_bytes.to_vec())?);
+                                bail!("Failed to get unicode for character {:?}", cid);
                             }
                             Some(b) => {
                                 return Ok(0x0100 * b[0] as u16 + b[1] as u16);
@@ -264,4 +322,11 @@ fn unicodify(
         text,
     })
     // todo!()
+}
+
+fn zlib_inflate(buf: &[u8]) -> Result<Vec<u8>> {
+    let mut z = ZlibDecoder::new(buf);
+    let mut inflated = Vec::<u8>::new();
+    z.read_to_end(&mut inflated)?;
+    Ok(inflated)
 }
